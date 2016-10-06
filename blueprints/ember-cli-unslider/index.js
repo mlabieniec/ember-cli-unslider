b3bfa332fe419ede0b44e1fd5df0839e1f464257
Michael Labieniec <mlabieniec@users.noreply.github.com>
2016-10-06 15:27:46 -0400

    Update index.js

--- blueprints/ember-cli-unslider/index.js
+++ blueprints/ember-cli-unslider/index.js
@@ -5,7 +5,6 @@ module.exports = {
 
   afterInstall: function(options) {
     return this.addBowerPackagesToProject([
-      { name: 'jquery.event.move', target: '^1.3.6' }, 
       { name: 'unslider',          source: 'https://github.com/idiot/unslider.git#master' }
     ]);    
   }
