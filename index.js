53233a78e12ec3abd5acadcba010c921ec66bcfe
Michael Labieniec <mlabieniec@users.noreply.github.com>
2016-10-06 15:41:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    app.import(app.bowerDirectory + '/jquery.event.move/js/jquery.event.move.js');
     app.import(app.bowerDirectory + '/unslider/src/js/unslider.js');
     app.import(app.bowerDirectory + '/unslider/dist/css/unslider.css');    
     app.import(app.bowerDirectory + '/unslider/dist/css/unslider-dots.css');    
